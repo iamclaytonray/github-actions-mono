68ceacf389359d17a598bf803a603037bd5722a8
Clayton Ray <cray@circadianrisk.com>
2020-01-02 11:08:37 -0500

    feat: initial commit

---
+++ server/test.js
@@ -0,0 +1,5 @@
+describe("Server", () => {
+  it("should add things", () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
---
+++ web/test.js
@@ -0,0 +1,5 @@
+describe("Web", () => {
+  it("should add all the things", () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
